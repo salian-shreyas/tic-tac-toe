ce904f55d4a2e8998f1ec407a0d645196c1dd264
Shreyas Salian <shreyassalian925@gmail.com>
2021-08-13 11:42:22 +0530

    Prompt for choice

--- scripts/main.rb
+++ scripts/main.rb
@@ -1,8 +1,9 @@
-require 'pry-byebug'
+# frozen_string_literal: true
 
+# Game module
 module Game
   def self.create_player(player)
-    player_info = self.prompt_player(player)
+    player_info = prompt_player(player)
 
     Player.new(player_info.first, player_info.last)
   end
@@ -10,7 +11,7 @@ module Game
   def self.prompt_player(player)
     print "Player #{player}, enter your name: "
     player_name = gets.chomp
-    mark = (player == 1) ? 'X' : 'O'
+    mark = player == 1 ? 'X' : 'O'
 
     puts "Hello, #{player_name}! Your mark is '#{mark}'"
 
@@ -21,47 +22,99 @@ module Game
     puts "Welcome to Tic-Tac-Toe Game\n"
   end
 
+  def self.winner?
+    false
+  end
 end
 
-
+# Class representing tic-tac-toe board
 class Board
-  @@data = [
+  attr_reader :data
+
+  def initialize
+    @data = [
       [1, 2, 3],
       [4, 5, 6],
       [7, 8, 9]
-  ]
+    ]
+  end
 
-  def self.display
-    @@data.each_with_index do |row, row_idx|
-      
+  def display
+    data.each_with_index do |row, row_idx|
+      print ' ' * 10
+
       row.each_with_index do |item, item_idx|
         print " #{item} "
-        print "|" unless item_idx == 2
+        print '|' unless item_idx == 2
       end
-    
-      puts "\n---|---|---" unless row_idx == 2
+
+      print "\n#{' ' * 10}"
+      puts '---|---|---' unless row_idx == 2
     end
 
-    puts ""
+    puts ''
   end
 
-  def self.edit(row_idx, col_idx, value)
-    @@data[row_idx][col_idx] = value
+  def edit(number, value)
+    index = index_of number
+    data[index.first][index.last] = value
   end
 
+  def index_of(number)
+    number -= 1
+    [number / 3, number % 3] # row index, col index
+  end
+
+  def value_at(number)
+    index = index_of number
+    value_at_index(index.first, index.last)
+  end
+
+  def value_at_index(row_idx, col_idx)
+    data[row_idx][col_idx]
+  end
 end
 
+# Class representing player
 class Player
+  attr_reader :name, :mark
+
   def initialize(name, mark)
-    @name = name
-    @mark = mark
+    @name = name.capitalize
+    @mark = mark.capitalize
+  end
+
+  def prompt(board)
+    puts "#{name}'s turn"
+    loop do
+      print "Choose one from above available number to mark '#{mark}': "
+      choice = gets.chomp.to_i
+      break if valid_choice?(choice, board)
+      print "#{choice} is not available!"
+    end
+    choice
+  end
+
+  def valid_choice?(choice, board)
+    value = board.value_at(choice)
+    return false if choice.between?(1, 9) || ['X', 'O'].include? value
+    return true
   end
 end
 
-
+# Start game
 Game.welcome
+
+board = Board.new
 
 player1 = Game.create_player 1
 player2 = Game.create_player 2
 
-Board.display
+until Game.winner?
+  board.display
+
+  choice = player1.prompt(board)
+  board.edit(choice, 'X')
+  choice = player2.prompt(board)
+  board.edit(choice, 'O')
+end
